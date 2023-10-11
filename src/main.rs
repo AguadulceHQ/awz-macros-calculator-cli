dd1064e8cf27dbde267132b9e6503d5e856ffa64
Luca Yesupatham Daniel <lucaydaniel@gmail.com>
2023-10-11 21:07:37 +0700

    feat(macros-calculator): migrate previous `main` into a module and calling it from `main`

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use crate::macros_calculator::{Activity, Gender, Person};
+
+pub mod macros_calculator;
+
 fn main() {
-    println!("Hello, world!");
+    let luca = Person::new(
+        String::from("Luca"),
+        33,
+        Gender::Male,
+        173,
+        70,
+        Activity::SuperActive,
+    );
+
+    println!("{}", luca);
 }
